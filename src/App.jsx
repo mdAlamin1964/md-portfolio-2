c84722b3b9449f219b183be14df03012a46f405f
mdalamin1964 <mdalamincoder@gmail.com>
2024-09-15 20:19:12 +0600

    first comment

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from "./Header"
+
+function App() {
+
+  return (
+    <>  
+      <Router>
+        <Header/>
+          <div className="main">
+            <Routes>
+              <Route path="/home" element={
+                <h1>Home</h1>
+              }/>
+              <Route path='/about' element={
+                  <h1>About</h1>
+                }/>
+              <Route path="/contact" element={
+                <h1>Contact</h1>
+              } />
+            </Routes>
+          </div>
+      </Router>
+    </>
+  )
+}
+ 
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+export default function Header() {
+    return (
+        <>
+            <div className="main-header">
+                <ul>
+                    <li><a href="home">Home</a></li>
+                    <li><a href="about">About</a></li>
+                    <li><a href="contact">Contact</a></li>
+                </ul>
+            </div>
+        </>
+    )
+}
