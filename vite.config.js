d5ccd94dc84852945d549f3e559b31d88ff2dd8d
mdalamin1964 <mdalamincoder@gmail.com>
2024-09-15 21:29:29 +0600

    upload 4

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/md-portfolio-2',
-  plugins: [react()],
+  plugins: [react(), ghPages()],
 })
